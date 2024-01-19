bf97593ed02dadce599cab339e1576619bc35ae5
jiwon <zentle7270@naver.com>
2024-01-19 17:20:49 +0900

    중고거래 테이블 작성

--- ZIBI/src/main/webapp/sql/jiwon.sql
+++ ZIBI/src/main/webapp/sql/jiwon.sql
@@ -1,5 +1,5 @@
 --중고거래
-create table secondhand(
+create table second(
  sc_num number not null,
  sc_title varchar2(150) not null,
  sc_content clob not null,
@@ -9,5 +9,85 @@ create table secondhand(
  sc_way number(1) not null,
  sc_place varchar2(50),
  sc_hit number(9) default 0,
- sc_favcount number(9) default 0
+ sc_favcnt number(9) default 0,
+ sc_reg_date date default sysdate not null,
+ sc_modify_date date,
+ sc_filename varchar2(200),
+ sc_ip varchat2(40) not null,
+ sc_show number(1) default 2 not null,
+ mem_num number not null,
+ constraint second_pk primary key (mem_num),
+ constraint second_fk foreign key (mem_num) references member (mem_num)
 );
+ 
+create sequence second_seq;
+ 
+-- 중고거래 거래 내역
+create table secondhand_order(
+ sc_order_num number not null,
+ sc_buyer varchar2(30) not null,
+ sc_order_reg_date date default sysdate not null,
+ sc_order_status number(1) default 1 not null,
+ sc_num number not null,
+ constraint second_order_pk primary key (sc_order_num),
+ constraint second_order_fk foreign key (sc_num) references secondhand (sc_num)
+);
+ 
+create sequence second_order_seq;
+
+--내 동네
+create table second_area(
+ sc_area varchar2(70),
+ mem_num number not null,
+ constraint second_area_fk foreign key (mem_num) references member (mem_num)
+);
+
+--중고거래 거래 후기
+create table second_review(
+ sc_rev_num number not null,      
+ sc_rev_star number(9,2) not null,
+ sc_rev_content varchar2(500),       
+ sc_num number not null,
+ mem_num number not null,
+ constraint second_review_pk primary key (sc_rev_num),
+ constraint second_review_fk1 foreign key (sc_num) references second (sc_num)
+ constraint second_review_fk2 foreign key (reviewer_num) references member (mem_num)
+);
+
+--중고거래 관심
+create table second_fav(
+ sc_num number not null,
+ mem_num number not null,
+ constraint fav_second_fk1 foreign key (sc_num) references spboard (sc_num),
+ constraint fav_pmember_fk2 foreign key (mem_num) references spmember (mem_num)
+);
+
+
+--채팅
+create table chatroom(
+ chatroom_num number not null,
+ sc_num number not null,
+ seller_num number not null,
+ buyer_num number not null,
+ constraint chatroom_pk primary key (chatroom_num),
+ constraint chatroom_fk1 foreign key (sc_num) references second (sc_num)
+ constraint chatroom_fk2 foreign key (seller_num) references member (mem_num),
+ constraint chatroom_fk3 foreign key (buyer_num) references member (mem_num)
+ 
+
+);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

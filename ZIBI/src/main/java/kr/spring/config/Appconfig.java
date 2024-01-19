da73afded8db84fdc31ae00c052dfdf7d214732d
rlad0 <rlad0@192.168.0.27>
2024-01-19 14:54:04 +0900

    first up

---
+++ ZIBI/src/main/java/kr/spring/config/Appconfig.java
@@ -0,0 +1,64 @@
+package kr.spring.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.view.tiles3.TilesConfigurer;
+import org.springframework.web.servlet.view.tiles3.TilesView;
+import org.springframework.web.servlet.view.tiles3.TilesViewResolver;
+
+import kr.spring.interceptor.LoginCheckInterceptor;
+
+@Configuration
+public class Appconfig implements WebMvcConfigurer {
+	
+	/*---------------------------로그인 interceptor----------------------------*/
+	private LoginCheckInterceptor loginCheck;
+	
+	@Bean
+	public LoginCheckInterceptor interceptor() { //인터셉터 객체 생성
+		loginCheck = new LoginCheckInterceptor();
+		return loginCheck;
+	}
+	
+	@Override
+	public void addInterceptors(InterceptorRegistry registry) { //인터셉터 등록
+		
+		registry.addInterceptor(loginCheck)
+			.addPathPatterns("/member/mypageMain")
+			.addPathPatterns("/member/mypageUpdate");
+	}
+	
+	
+	/*---------------------------Tiles 사용 설정----------------------------*/
+	@Bean
+	public TilesConfigurer tiesConfigurer() {
+		final TilesConfigurer configurer = new TilesConfigurer();
+		
+		//tilesdef.xml의 경로와 파일명 지정
+		configurer.setDefinitions(new String[] { //여러 개 넘겨주기 위해 배열 이용
+					"/WEB-INF/tiles-def/main.xml",
+					"/WEB-INF/tiles-def/de.xml",
+					"/WEB-INF/tiles-def/hyun.xml",
+					"/WEB-INF/tiles-def/jiwon.xml",
+					"/WEB-INF/tiles-def/jy.xml",
+					"/WEB-INF/tiles-def/na.xml",
+					"/WEB-INF/tiles-def/yeeun.xml"
+					}); 
+		configurer.setCheckRefresh(true);
+		
+		return configurer;
+	} 
+	
+	@Bean
+	public TilesViewResolver tilesViewResolver() {
+		
+		final TilesViewResolver tilesViewResolver = new TilesViewResolver();
+		tilesViewResolver.setViewClass(TilesView.class);
+		
+		return tilesViewResolver;
+	}
+	
+	
+}

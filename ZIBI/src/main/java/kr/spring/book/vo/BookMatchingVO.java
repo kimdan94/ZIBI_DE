b27f4ec53b065bdc6bf388602b32bb41b6131598
Anna Cho <rema1ny0un9@gmail.com>
2024-01-21 16:30:48 +0900

    BookMatchingVO update

--- ZIBI/src/main/java/kr/spring/book/vo/BookMatchingVO.java
+++ ZIBI/src/main/java/kr/spring/book/vo/BookMatchingVO.java
@@ -17,4 +17,9 @@ public class BookMatchingVO {
 	@NotBlank
 	private int book_state;//0 : 대기, 1 : 확정, 2 : 거절, 3 : 취소
 	private Date book_matchDate;//SYSDATE(default)
+	
+	/*-- JOIN으로 읽어올 정보 --*/
+	private String mem_name;
+	private String mem_email;
+	private String mem_phone;
 }

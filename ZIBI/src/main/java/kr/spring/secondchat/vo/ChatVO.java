9e48111b1faaa5d99b3196a6b719bca49b08e4d3
jiwon <zentle7270@naver.com>
2024-01-31 17:10:30 +0900

    중고거래 채팅 ChatVO 수정

--- ZIBI/src/main/java/kr/spring/secondchat/vo/ChatVO.java
+++ ZIBI/src/main/java/kr/spring/secondchat/vo/ChatVO.java
@@ -12,10 +12,10 @@ import lombok.ToString;
 public class ChatVO {
 	private int chat_num;			//채팅 메시지 식별 번호//pk
 	private String chat_message;	//채팅 메시지 내용
-	private Date chat_reg_date;		//채팅 등록일
-	private int chat_read_check;	//메시지 읽음 체크(0:안읽음, 1:읽음)
+	private String chat_reg_date;		//채팅 등록일
+	private int chat_read_check;	//메시지 읽음 체크(0:읽음, 1:안읽음)
 	private int chatroom_num;		//채팅방 식별 번호//fk
 	private int mem_num;			//회원 식별 번호//fk
 	
-	
+	private int read_count;			//읽지 않은 메시지수
 }
